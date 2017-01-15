5a44babe2598f8442699d5535e30fa8015600542
Raheel Anwar <raheelanwar74@gmail.com>
2017-01-15 09:58:17 +0500

    Add files via upload

---
+++ MetroCleaner/Classes/UtilityClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+using System.IO;
+
+namespace MetroCleaner
+{
+    class UtilityClass
+    {
+        public static string systemInfo()
+        {
+            StringBuilder sysInfo = new StringBuilder();
+
+            sysInfo.Append(String.Format("Current User: {0}\n", Environment.UserName));
+            sysInfo.Append(String.Format("Machine Name: {0}\n", Environment.MachineName));
+            sysInfo.Append(String.Format("User Profile: {0}\n", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)));
+            sysInfo.Append(String.Format("System Folder: {0}\n", Environment.SystemDirectory));
+            sysInfo.Append(String.Format("Processor Count: {0} Cores\n", Environment.ProcessorCount));
+
+            return sysInfo.ToString();
+        }
+
+        public static void customAnimation(Window win, string animationName, DependencyObject obj)
+        {
+            Storyboard sb = win.FindResource(animationName) as Storyboard;
+            Storyboard.SetTarget(sb, obj);
+            sb.Begin();
+        }
+
+        public static void deleteAll(string path)
+        {
+            String[] files = Directory.GetFiles(path);
+            String[] directory = Directory.GetDirectories(path);
+
+            foreach (string file in files)
+            {
+                try
+                {
+                    File.Delete(file);
+                }
+                catch
+                {
+                    //TODO add a log file for this.
+                    //Just skip is a file can not be deleted.
+                }
+
+            }
+
+            foreach (string dir in directory)
+            {
+                try
+                {
+                    Directory.Delete(dir, true);
+                }
+                catch
+                {
+                    //TODO add a log file for this.
+                    //Just skip if a directory can not be deleted.
+                }
+            }
+        }
+    }
+}
---
+++ MetroCleaner/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MetroCleaner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                txtSysDetails.Text = UtilityClass.systemInfo();
+            }
+            catch
+            {
+                MessageBox.Show("There is a problem in detecting your system information. This app may not be compatible with your OS.\nFor more information please read instruction manual.", "Error Detecting SystemInfo", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Close();
+            }
+        }
+
+        private async void btnClean_Click(object sender, RoutedEventArgs e)
+        {
+            btnClean.IsEnabled = false;
+            txtTaskDetails.Text = "";
+
+            if (imgGlass.Visibility == Visibility.Visible)
+            {
+                UtilityClass.customAnimation(this, "fadeOut", imgGlass);
+                UtilityClass.customAnimation(this, "fadeOut", txtGlass);
+                await Task.Delay(600);
+
+                imgGlass.Visibility = Visibility.Hidden;
+                txtGlass.Visibility = Visibility.Hidden;
+            }
+            else if (imgPass.Visibility == Visibility.Visible)
+            {
+                UtilityClass.customAnimation(this, "fadeOut", imgPass);
+                UtilityClass.customAnimation(this, "fadeOut", txtPass);
+                await Task.Delay(600);
+
+                imgPass.Visibility = Visibility.Hidden;
+                txtPass.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                UtilityClass.customAnimation(this, "fadeOut", imgFail);
+                UtilityClass.customAnimation(this, "fadeOut", txtFail);
+                await Task.Delay(600);
+
+                imgFail.Visibility = Visibility.Hidden;
+                txtFail.Visibility = Visibility.Hidden;
+            }
+
+            try
+            {
+                //System cleaning main section
+                imgLoading.Visibility = Visibility.Visible;
+                txtLoading.Visibility = Visibility.Visible;
+
+                UtilityClass.customAnimation(this, "fadeIn", imgLoading);
+                UtilityClass.customAnimation(this, "fadeIn", txtLoading);
+                await Task.Delay(600);
+
+                txtTaskDetails.Text = "Initializing process...\n";
+                txtTaskDetails.Text += "Calculating files...\n";
+                txtTaskDetails.Text += "Start Cleaning...\n";
+
+                String tempPath = System.IO.Path.GetTempPath();
+                UtilityClass.deleteAll(tempPath);
+
+                txtTaskDetails.Text += "Cleaning successful...";
+
+                UtilityClass.customAnimation(this, "fadeOut", imgLoading);
+                UtilityClass.customAnimation(this, "fadeOut", txtLoading);
+                await Task.Delay(600);
+
+                imgLoading.Visibility = Visibility.Hidden;
+                txtLoading.Visibility = Visibility.Hidden;
+                //=============================================//
+
+                imgPass.Visibility = Visibility.Visible;
+                txtPass.Visibility = Visibility.Visible;
+
+                UtilityClass.customAnimation(this, "fadeIn", imgPass);
+                UtilityClass.customAnimation(this, "fadeIn", txtPass);
+                await Task.Delay(600);
+            }
+            catch
+            {
+                txtTaskDetails.Text += "Cleaning failed...\n";
+                imgFail.Visibility = Visibility.Visible;
+                txtFail.Visibility = Visibility.Visible;
+
+                UtilityClass.customAnimation(this, "fadeIn", imgFail);
+                UtilityClass.customAnimation(this, "fadeIn", txtFail);
+                await Task.Delay(600);
+            }
+
+            btnClean.IsEnabled = true;
+        }
+
+        
+    }
+}
